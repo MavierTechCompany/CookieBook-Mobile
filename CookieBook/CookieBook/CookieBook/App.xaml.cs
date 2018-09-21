8885805ddf7eb7424a3830265630af85784c5a4d
Michael B <MadTiger2409@users.noreply.github.com>
2018-09-21 23:35:40 +0200

    Working on login page. This should be on branch but I was working on master by mistake.

--- CookieBook/CookieBook/CookieBook/App.xaml.cs
+++ CookieBook/CookieBook/CookieBook/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookieBook.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace CookieBook
 		{
 			InitializeComponent();
 
-			MainPage = new CookieBook.MainPage();
+            MainPage = new LoginPage();
 		}
 
 		protected override void OnStart ()
