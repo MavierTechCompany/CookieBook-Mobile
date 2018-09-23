158de3020a0de0daf10b45e1ba2095dbe675d553
Michael B <MadTiger2409@users.noreply.github.com>
2018-09-23 17:50:02 +0200

    Added custom rendered for button on android.

---
+++ CookieBook/CookieBook/CookieBook.Android/Renderers/CustomButtonRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CookieBook.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomButtonRenderer))]
+namespace CookieBook.Droid.Renderers
+{
+    class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            var button = this.Control;
+            button.SetAllCaps(false);
+        }
+    }
+}
